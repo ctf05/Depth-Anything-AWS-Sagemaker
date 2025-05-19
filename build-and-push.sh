42fda153b5ea3d10b55a5f008020eb789076ce16
ctf05 <christianbeadling@protonmail.com>
2025-05-19 00:08:48 -0400

    new repo

--- build-and-push.sh
+++ build-and-push.sh
@@ -4,10 +4,10 @@ echo "Pulling repo"
 git pull
 
 echo "Starting Docker build process..."
-docker build --no-cache -t lome/depthanythingsagemaker .
+docker build --no-cache -t lome/depthanythingsagemaker-NewArch .
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 181172696166.dkr.ecr.us-east-2.amazonaws.com
-docker tag lome/depthanythingsagemaker:latest 181172696166.dkr.ecr.us-east-2.amazonaws.com/lome/depthanythingsagemaker:latest
-docker push 181172696166.dkr.ecr.us-east-2.amazonaws.com/lome/depthanythingsagemaker:latest
+docker tag lome/depthanythingsagemaker-NewArch:latest 181172696166.dkr.ecr.us-east-2.amazonaws.com/lome/depthanythingsagemaker-NewArch:latest
+docker push 181172696166.dkr.ecr.us-east-2.amazonaws.com/lome/depthanythingsagemaker-NewArch:latest
 
 echo "Process completed successfully!"
 echo
