2211ca42d983212f1f218314a2a341834aeac30a
ctf05 <christianbeadling@protonmail.com>
2025-05-20 11:24:02 -0400

    fix

--- build-and-push.sh
+++ build-and-push.sh
@@ -4,7 +4,7 @@ echo "Pulling repo"
 git pull
 
 echo "Starting Docker build process..."
-docker build --no-cache -t lome/depthanythingsagemaker-newarch .
+docker build -t lome/depthanythingsagemaker-newarch .
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 181172696166.dkr.ecr.us-east-2.amazonaws.com
 docker tag lome/depthanythingsagemaker-newarch:latest 181172696166.dkr.ecr.us-east-2.amazonaws.com/lome/depthanythingsagemaker-newarch:latest
 docker push 181172696166.dkr.ecr.us-east-2.amazonaws.com/lome/depthanythingsagemaker-newarch:latest
